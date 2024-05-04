7f63b08a0d87113f9966ba9597d37cf1b26936ea
Micheal Soyemi <mshow1980@aol.com>
2024-05-03 18:23:41 -0600

    another update

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.203.33.53:9000//',
+  serverUrl: 'http://54.90.216.165:9000//',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'YouTube-Skit',
 	    'sonar.projectKey':'YouTube-Skit',
 	    //'sonar.login': ',
 	    'sonar.login': 'YWRtaW4=',
-	    'sonar.password': 'YWRtaW4=',
+	    'sonar.password': 'Q29udHJvbDEw',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
